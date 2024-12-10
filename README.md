1ffe6e0bdfc59aa2b5f89452babfcba16419de80
Bradley Mclain <bmclain@firemate.com>
2024-12-10 17:40:46 +1100

    adds script for converting batches of mp4 to webp

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # martin
 Batch converter from mp4 to webp
+
+# Setup
+
+## Windows
+
+1. Install python3 from https://www.python.org/
+
+# Usage
+
+1. Place source files in `in` directory
+
+2. Run convert script
+    ```
+    py convert.py
+    ```
---
+++ convert.py
@@ -0,0 +1,48 @@
+from pathlib import Path
+import subprocess
+
+ffmpeg_path = Path(".", "ffmpeg-2024-12-09", "bin", "ffmpeg")
+ffprobe_path = Path(".", "ffmpeg-2024-12-09", "bin", "ffprobe")
+
+in_path = Path(".", "in")
+in_path.mkdir(parents=True, exist_ok=True)
+
+out_path = Path(".", "out")
+out_path.mkdir(parents=True, exist_ok=True)
+
+convered = 0
+
+videos = Path(".", "in").rglob('*.mp4')
+for video in videos:
+
+    size = subprocess.run([
+        ffprobe_path,
+        "-v", "error",
+        "-select_streams", "v:0",
+        "-show_entries", "stream=width,height",
+        "-of", "csv=s=x:p=0",
+        video
+    ], capture_output = True, text=True).stdout
+    width, height = size.strip().split("x")
+
+    webp_path = Path(out_path, f"{video.name.replace(".mp4", "")}.webp")
+    return_code = subprocess.run([
+        ffmpeg_path,
+        "-i", video,
+        "-vcodec", "libwebp",
+        "-filter:v", "fps=fps=20",
+        "-lossless", "1",
+        "-loop", "0",
+        "-preset", "default",
+        "-an",
+        "-vsync", "0",
+        "-s", f"{width}:{height}",
+        "-y", # overwrite
+        webp_path
+    ])
+
+    print(return_code)
+
+    convered += 1
+
+print(f"\nconverted {convered} videos to webp!")
