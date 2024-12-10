e679beaf09f42d6af49ca54baaa94103fde1dd33
Bradley Mclain <bmclain@firemate.com>
2024-12-10 17:51:59 +1100

    adds option for setting fps

--- convert.py
+++ convert.py
@@ -1,48 +1,60 @@
+import argparse
 from pathlib import Path
 import subprocess
 
-ffmpeg_path = Path(".", "ffmpeg-2024-12-09", "bin", "ffmpeg")
-ffprobe_path = Path(".", "ffmpeg-2024-12-09", "bin", "ffprobe")
+parser = argparse.ArgumentParser(
+    prog="martin",
+    description="batch convert mp4 to webp using ffmpeg"
+)
+parser.add_argument('--fps', default=25, required=False)
+args = parser.parse_args()
 
-in_path = Path(".", "in")
-in_path.mkdir(parents=True, exist_ok=True)
+def run():
+    ffmpeg_path = Path(".", "ffmpeg-2024-12-09", "bin", "ffmpeg")
+    ffprobe_path = Path(".", "ffmpeg-2024-12-09", "bin", "ffprobe")
 
-out_path = Path(".", "out")
-out_path.mkdir(parents=True, exist_ok=True)
+    in_path = Path(".", "in")
+    in_path.mkdir(parents=True, exist_ok=True)
 
-convered = 0
+    out_path = Path(".", "out")
+    out_path.mkdir(parents=True, exist_ok=True)
 
-videos = Path(".", "in").rglob('*.mp4')
-for video in videos:
+    convered = 0
 
-    size = subprocess.run([
-        ffprobe_path,
-        "-v", "error",
-        "-select_streams", "v:0",
-        "-show_entries", "stream=width,height",
-        "-of", "csv=s=x:p=0",
-        video
-    ], capture_output = True, text=True).stdout
-    width, height = size.strip().split("x")
+    videos = Path(".", "in").rglob('*.mp4')
+    for video in videos:
 
-    webp_path = Path(out_path, f"{video.name.replace(".mp4", "")}.webp")
-    return_code = subprocess.run([
-        ffmpeg_path,
-        "-i", video,
-        "-vcodec", "libwebp",
-        "-filter:v", "fps=fps=20",
-        "-lossless", "1",
-        "-loop", "0",
-        "-preset", "default",
-        "-an",
-        "-vsync", "0",
-        "-s", f"{width}:{height}",
-        "-y", # overwrite
-        webp_path
-    ])
+        size = subprocess.run([
+            ffprobe_path,
+            "-v", "error",
+            "-select_streams", "v:0",
+            "-show_entries", "stream=width,height",
+            "-of", "csv=s=x:p=0",
+            video
+        ], capture_output = True, text=True).stdout
+        width, height = size.strip().split("x")
 
-    print(return_code)
+        webp_path = Path(out_path, f"{video.name.replace(".mp4", "")}.webp")
+        return_code = subprocess.run([
+            ffmpeg_path,
+            "-i", video,
+            "-vcodec", "libwebp",
+            "-filter:v", f"fps=fps={args.fps}",
+            "-lossless", "1",
+            "-loop", "0",
+            "-preset", "default",
+            "-an",
+            "-vsync", "0",
+            "-s", f"{width}:{height}",
+            "-y", # overwrite
+            webp_path
+        ])
 
-    convered += 1
+        print(return_code)
 
-print(f"\nconverted {convered} videos to webp!")
+        convered += 1
+
+    print(f"\nconverted {convered} videos to webp!")
+
+if __name__ == "__main__":
+    run()
